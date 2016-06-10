fb2790d8d695228463f768bfc20aead458fd5df4
sushmai <maryadareddy@gmail.com>
2016-06-09 20:55:42 -0700

    Add files via upload

---
+++ name.rb
@@ -0,0 +1,20 @@
+def print_two(*args)
+	arg1, arg2 = args
+	puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def print_two_again(arg1, arg2)
+	puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def print_one(arg1)
+	puts "arg1: #{arg1}"
+end
+
+def print_none()
+	puts "i got nothing"
+end
+
+print_two("hi","hello")
+print_one("First!")
+print_none()
